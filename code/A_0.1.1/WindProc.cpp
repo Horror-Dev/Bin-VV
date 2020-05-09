05f3326c60e8aca0be55e21ddae3f1de1865ac8e
horror_dev <33747130+shadowGamming@users.noreply.github.com>
2020-05-09 08:54:32 +0500

    Add files via upload

---
+++ code/A_0.1.1/WindProc.cpp
@@ -0,0 +1,48 @@
+#include"BinApp.h"
+
+// Window Processes 
+LRESULT CALLBACK BinApp::WindProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+	switch (uMsg) {
+	case WM_CREATE:
+		this->onCreate();
+		break;
+	case WM_DESTROY:
+		this->onClose();
+		PostQuitMessage(0);
+		return 0;
+	case WM_COMMAND:
+		this->Command(LOWORD(wParam));
+		return 0;
+	case WM_MOVE:
+		this->onMove();
+		break;
+	case WM_NCHITTEST:
+		SetWindowLong(hwnd, DWL_MSGRESULT, HTCAPTION);
+		return HTCAPTION;
+	case WM_NCCALCSIZE:
+		if (wParam == TRUE)
+			SetWindowLong(hwnd, DWL_MSGRESULT, 0);
+		return TRUE;
+		return FALSE;
+	break;
+	case WM_PAINT:
+	case WM_DISPLAYCHANGE:
+		this->Paint(hwnd);
+		break;
+	case WM_DRAWITEM:
+	//	MessageBox(NULL, L"rt", L"cap", ERROR);
+		this->ButtonDrawing(wParam,reinterpret_cast<PDRAWITEMSTRUCT>(lParam));
+		return 0;
+	case WM_SETCURSOR:
+	{
+		HWND wind = reinterpret_cast<HWND>(wParam);
+		if (wind == this->CloseButtonHwnd || wind == this->MiniMizeButtonHwnd || wind == this->TranslateButtonHwnd)
+		{
+			SetCursor(LoadCursor(NULL, IDC_HAND));
+			return(TRUE);
+		}
+	}
+	break;
+	}
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
