92af195445a41d1c0fc4ee40baa9cf062881b868
horror_dev <33747130+shadowGamming@users.noreply.github.com>
2020-05-09 08:50:24 +0500

    Rename code/Events.cpp to code/A_0.1/Events.cpp

--- code/A_0.1/Events.cpp
+++ code/A_0.1/Events.cpp
@@ -8,7 +8,7 @@ void BinApp::onCreate() {
 	catch (const std::exception& ex) {
 		std::string ErrInfo = ex.what();
 
-		MessageBox(nullptr, std::wstring(begin(ErrInfo), end(ErrInfo)).c_str(), L"Œ¯Ë·Í‡", MB_ICONERROR | MB_OK);
+		MessageBox(nullptr, std::wstring(begin(ErrInfo), end(ErrInfo)).c_str(), L"√é√∏√®√°√™√†", MB_ICONERROR | MB_OK);
 		ExitProcess(EXIT_FAILURE);
 	}
 }
