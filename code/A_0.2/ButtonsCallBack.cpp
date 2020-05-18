9190b8714d900b8a940f21097e6f13d5ed239e52
horror_dev <33747130+Horror-Dev@users.noreply.github.com>
2020-05-18 22:25:08 +0500

    Update ButtonsCallBack.cpp

--- code/A_0.2/ButtonsCallBack.cpp
+++ code/A_0.2/ButtonsCallBack.cpp
@@ -38,24 +38,14 @@ void BinApp::onTransfButton(LPARAM lParam) {
 		GetWindowText(this->EditHWND, &text[0], MAX_PATH);
 		text.erase(remove(begin(text), end(text), 0), end(text));
 		if (text.empty()) {
-			MessageBox(this->MainHwnd, L"Âåäèòå ÷èñëî", L"Èíôîðìàöèÿ", MB_ICONINFORMATION | MB_OK);
+			MessageBox(this->MainHwnd, L"Ã‚Ã¥Ã¤Ã¨Ã²Ã¥ Ã·Ã¨Ã±Ã«Ã®", L"ÃˆÃ­Ã´Ã®Ã°Ã¬Ã Ã¶Ã¨Ã¿", MB_ICONINFORMATION | MB_OK);
 			return;
 		}
-		//switch (this->NumberSystem)
-		//{
-		//case 2:
-		//	text = to2(text);
-		//	break;
-		//case 16:
-		//	text = to16(text);
-		//default:
-		//	break;
-		//}
 		text = Convert(text, NumSystem, Num2System);
 		SetWindowText(this->EditHWND, text.c_str());
 	}
 	catch (...) {
-		MessageBox(this->MainHwnd, L"÷èñëî ïðåâûøàåò 2 áàéòà", L"Âíèìàíèå", MB_ICONINFORMATION | MB_OK);
+		MessageBox(this->MainHwnd, L"Ã·Ã¨Ã±Ã«Ã® Ã¯Ã°Ã¥Ã¢Ã»Ã¸Ã Ã¥Ã² 2 Ã¡Ã Ã©Ã²Ã ", L"Ã‚Ã­Ã¨Ã¬Ã Ã­Ã¨Ã¥", MB_ICONINFORMATION | MB_OK);
 	}
 }
 
